43c45c5c6df26c6dfc64db09213b5c46cc9289c1
Yamean0013 <yamean1089@gmail.com>
2026-04-14 06:34:53 +0500

    Create posts library and create types

---
+++ src/lib/posts.ts
@@ -0,0 +1,25 @@
+export type Post = {
+  slug: string;
+  title: string;
+  date: string;
+  content: string;
+};
+
+export const posts: Post[] = [
+  {
+    slug: "Hello-World",
+    title: "Hellow World",
+    date: "2026-04-14",
+    content: "This is my very first blog post. Welcome!",
+  },
+  {
+    slug: "building-apps",
+    title: "Building Apps",
+    date: "2026-04-14",
+    content: "A guide on building real world applications",
+  },
+];
+
+export function getPostBySlug(slug: string): Post | undefined {
+  return posts.find((post) => post.slug === slug);
+}
