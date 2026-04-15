c1f795b709dc67fa00881981fd5f03d256b83908
Yamean0013 <yamean1089@gmail.com>
2026-04-15 21:43:59 +0500

    Add posts and description

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,24 @@ import { Badge } from "@/components/ui/badge";
 export default function HomePage() {
   return (
     <main className="bg-gray-900 text-white">
-      <section className="flex items-start justify-center pt-5">
+      <section className="flex items-start justify-center py-20">
         <div className="text-center">
           <h1 className="text-4xl font-bold">The DEV Blog</h1>
           <p className="text-sm">Get technical, Get Deep</p>
         </div>
       </section>
 
-      <section className="flex h-screen justify-center py-20">
+      <section className="flexh h-screen justify-center pt-5">
         <div className="text-center">
-          <p>Latest Blogs</p>
+          <h2 className="font-bold">Latest Posts</h2>
+        </div>
+        <div className="grid grid-center">
+          {posts.map((post) => (
+            <div key={post.slug} className="mt-10">
+              <h2 className="text-2xl font-bold">{post.title}</h2>
+              <p className="text-xl">{post.content}</p>
+            </div>
+          ))}
         </div>
       </section>
     </main>
