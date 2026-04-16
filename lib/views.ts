a55d15784a5eb826f83558f5a28aecde4489d113
Yameen <yamean1089@gmail.com>
2026-04-16 15:07:33 +0500

    Add views file and create dashboard

---
+++ lib/views.ts
@@ -0,0 +1,16 @@
+const dailyView = [
+  { label: "Sunday", vistis: 1000 },
+  { label: "Monday", vistis: 1000 },
+  { label: "Tuesday", vistis: 1000 },
+  { label: "Wednesday", vistis: 1000 },
+  { label: "Thursday", vistis: 1000 },
+  { label: "Friday", vistis: 1000 },
+  { label: "Saturday", vistis: 1000 },
+];
+
+const weeklyView = [
+  { label: "Week 1", visits: 38400 },
+  { label: "Week 2", visits: 42100 },
+  { label: "Week 3", visits: 45200 },
+  { label: "Week 4", visits: 39800 },
+];
