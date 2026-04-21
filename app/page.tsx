98550ccc91b1994c87f9c098bdbf376837e9af71
Yamean0013 <yamean1089@gmail.com>
2026-04-21 21:21:21 +0500

    Add new styles and animations to homescreen

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,28 @@ export default function HomePage() {
     <main className="bg-gray-900 text-white">
       <div className="flex items-center justify-between text-2xl font-bold">
         <div>Logo</div>
-        <div className="flex gap-15 px-9">
-          <span>Home</span>
-          <span>About</span>
-          <span>Contact</span>
+        <div className="flex gap-15 px-4">
+          <span className="cursor-pointer hover:text-3xl transition-all">
+            Home
+          </span>
+          <span className="cursor-pointer hover:text-3xl transition-all">
+            About
+          </span>
+          <span className="cursor-pointer hover:text-3xl transition-all">
+            Contact
+          </span>
         </div>
       </div>
       <section className="flex flex-col justify-center py-20">
         <div className="text-center mx-auto">
-          <h1 className="text-4xl font-bold">The DEV Blog</h1>
+          <h1 className="text-4xl font-bold hover:text-slate-500">
+            The DEV Blog
+          </h1>
           <p className="text-sm">Get technical, Get Deep</p>
           <Button
             asChild
             variant="outline"
-            className="border-gray-700 hover:bg-gray-800 mt-5"
+            className="border-gray-700 hover:bg-gray-800 hover:text-xl mt-5"
           >
             <Link href={"/dashboard"}>Dashboard</Link>
           </Button>
