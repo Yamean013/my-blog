a18c971807e85c5d2e304b7ed670090b0d1703f0
Yamean0013 <yamean1089@gmail.com>
2026-04-22 06:00:28 +0500

    Updated image source

--- lib/posts.ts
+++ lib/posts.ts
@@ -19,7 +19,7 @@ export const posts: Post[] = [
     title: "Building Apps",
     date: "2026-04-14",
     content: "A guide on building real world applications",
-    url: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=3272&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
