4ef4cfd04bb8ed82b322590c3601a0a0c4e19f3a
Yamean0013 <yamean1089@gmail.com>
2026-04-23 06:32:40 +0500

    Add sidebar, header and menu items

--- app/page.tsx
+++ app/page.tsx
@@ -10,77 +10,101 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
+import {
+  Sidebar,
+  SidebarInset,
+  SidebarProvider,
+  SidebarContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 
 export default function HomePage() {
   return (
-    <main className="bg-gray-900 text-white">
-      <div className="flex items-center justify-between text-2xl font-bold">
-        <div>Logo</div>
-        <div className="flex gap-15 px-4">
-          <span className="cursor-pointer hover:text-3xl transition-all">
-            Home
-          </span>
-          <span className="cursor-pointer hover:text-3xl transition-all">
-            About
-          </span>
-          <span className="cursor-pointer hover:text-3xl transition-all">
-            Contact
-          </span>
-        </div>
-      </div>
-      <section className="flex flex-col justify-center py-20">
-        <div className="text-center mx-auto">
-          <h1 className="text-4xl font-bold hover:text-slate-500">
-            The DEV Blog
-          </h1>
-          <p className="text-sm">Get technical, Get Deep</p>
-          <Button
-            asChild
-            variant="outline"
-            className="border-gray-700 hover:bg-gray-800 hover:text-xl mt-5"
-          >
-            <Link href={"/dashboard"}>Dashboard</Link>
-          </Button>
-        </div>
-      </section>
-
-      <section className="h-screen justify-center pt-5">
-        <div className="text-center">
-          <h2 className="font-bold">Latest Posts</h2>
-        </div>
-        <div className="flex flex-col items-center w-full px-4">
-          {posts.map((post) => (
-            <div
-              key={post.slug}
-              className="flex gap-6 items-start mt-10 w-full max-w-2xl text-left"
+    <SidebarProvider className="bg-gray-900 text-white">
+      <Sidebar>
+        <SidebarHeader>
+          <h1 className="flex justify-center">Dev Blog</h1>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href={"./"}>Home</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href={"./"}>About</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href={"./"}>Contact</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarContent>
+      </Sidebar>
+      <SidebarInset>
+        <SidebarTrigger />
+        <section className="flex flex-col justify-center py-20">
+          <div className="text-center mx-auto">
+            <h1 className="text-4xl font-bold hover:text-slate-500">
+              The DEV Blog
+            </h1>
+            <p className="text-sm">Get technical, Get Deep</p>
+            <Button
+              asChild
+              variant="outline"
+              className="border-gray-700 hover:bg-gray-800 hover:text-xl mt-5"
             >
-              <div className="shrink-0">
-                <Image
-                  src={post.url}
-                  alt={post.title}
-                  width={160}
-                  height={120}
-                  className="rounded-lg object-cover"
-                ></Image>
-              </div>
-              <h2 className="text-2xl font-bold">{post.title}</h2>
-              <p className="text-xl">{post.content}</p>
+              <Link href={"/dashboard"}>Dashboard</Link>
+            </Button>
+          </div>
+        </section>
 
-              <div className="mt-6">
-                <Button
-                  asChild
-                  variant="outline"
-                  className="border-gray-700 hover:bg-gray-800"
-                >
-                  <Link href={`/blog/${post.slug}`}>Read More</Link>
-                </Button>
+        <section className="h-screen justify-center pt-5">
+          <div className="text-center">
+            <h2 className="font-bold">Latest Posts</h2>
+          </div>
+          <div className="flex flex-col items-center w-full px-4">
+            {posts.map((post) => (
+              <div
+                key={post.slug}
+                className="flex gap-6 items-start mt-10 w-full max-w-2xl text-left"
+              >
+                <div className="shrink-0">
+                  <Image
+                    src={post.url}
+                    alt={post.title}
+                    width={160}
+                    height={120}
+                    className="rounded-lg object-cover"
+                  ></Image>
+                </div>
+                <h2 className="text-2xl font-bold">{post.title}</h2>
+                <p className="text-xl">{post.content}</p>
+
+                <div className="mt-6">
+                  <Button
+                    asChild
+                    variant="outline"
+                    className="border-gray-700 hover:bg-gray-800"
+                  >
+                    <Link href={`/blog/${post.slug}`}>Read More</Link>
+                  </Button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
-    </main>
+            ))}
+          </div>
+        </section>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
