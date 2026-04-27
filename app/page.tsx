8ff0b955aa97f55e1d6bf87bf784f286c15e3673
Yamean0013 <yamean1089@gmail.com>
2026-04-27 05:58:57 +0500

    Create admin path and update routing

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function HomePage() {
             variant="outline"
             className="border-gray-700 hover:text-lg transition-all mt-5"
           >
-            <Link href={"/dashboard"}>Dashboard</Link>
+            <Link href={"/admin/dashboard/page.tsx"}>Dashboard</Link>
           </Button>
         </div>
       </section>
