2a2b3aa3259e33d702317bf30b8c75dbd2140693
Yameen <yamean1089@gmail.com>
2026-05-07 15:06:12 +0500

    Create separate layout for admin

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -16,34 +16,5 @@ import {
 import { Button } from "@/components/ui/button";
 
 export default function Page() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Build Your Application</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <section className="flex flex-1 flex-col gap-4 p-4">
-          <div className="flex justify-start">
-            <Button className="mx-2 mt-5 cursor-pointer">Add user</Button>
-            <Button className="hover:bg-red-500 transition-normal mx-2 mt-5 cursor-pointer">
-              Remove user
-            </Button>
-          </div>
-        </section>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <></>;
 }
