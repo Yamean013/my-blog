2d37988c9545706a67763139af2fa443e379b886
Yamean0013 <yamean1089@gmail.com>
2026-05-19 06:00:35 +0500

    Created footer and updated style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="fixed bottom-0 left-0 w-full z-50 py-4">
+      <div className="flex items-center justify-center">
+        @Coralbyt All Rights Reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
